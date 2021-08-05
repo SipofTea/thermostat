2c7e4b2bc23483a176c35915983e11ecf4d6542d
Else Meijerink <p.e.meijerink@gmail.com>
2021-08-05 22:06:04 +0100

    Implemented city temp

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -1,11 +1,20 @@
-fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" +
-      API_KEY +
-      "&units=metric"
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      document.querySelector("#outdoor-temp").innerText = data.main.temp;
-    });
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#city-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const cityName = document.querySelector("#city-name").value;
+
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        cityName +
+        "&appid=" +
+        API_KEY +
+        "&units=metric"
+      )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        document.querySelector("#outdoor-temp").innerText = data.main.temp;
+      });
+  });
+});
